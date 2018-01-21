d61c4475adffad2375b9627d1183274854366d49
loveKino <842914439@qq.com>
2018-01-22 00:16:00 +0800

    opt

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,30 +1,45 @@
 /**
  * compare regular expression and cl-fsm
  */
-var Benchmark = require('benchmark');
-var suite = new Benchmark.Suite;
+const Benchmark = require('benchmark');
 
-let {
+const {
     RegularExp
 } = require('..');
 
-// add tests 
-suite.add('RegExp', function() {
+const log = console.log; // eslint-disable-line
+
+{
+    // add tests
+    new Benchmark.Suite().add('RegExp-iteral', function() {
         /[abc]ed*/.test('aeddddddd');
-    }).add('RegExp', function() {
+    }).add('RegExp-new reg', function() {
         new RegExp('[abc]ed*').test('aedddddd');
-    })
-    .add('RegularExp', function() {
+    }).add('RegularExp', function() {
         new RegularExp('[abc]ed*').test('aeddddddd');
-    })
-    // add listeners 
-    .on('cycle', function(event) {
-        console.log(String(event.target));
-    })
-    .on('complete', function() {
-        console.log('Fastest is ' + this.filter('fastest').map('name'));
-    })
-    // run async 
-    .run({
+    }).on('cycle', function(event) {
+        log(String(event.target));
+    }).on('complete', function() {
+        log('Fastest is ' + this.filter('fastest').map('name'));
+    }).run({
         'async': true
     });
+}
+
+{
+    const regStr = '[abc]ed*';
+    const reg1 = new RegExp(regStr);
+    const reg2 = new RegularExp(regStr);
+
+    new Benchmark.Suite().add('test: raw RegExp', function() {
+        reg1.test('aedddddd');
+    }).add('test: cl-fsm RegularExp', function() {
+        reg2.test('aeddddddd');
+    }).on('cycle', function(event) {
+        log(String(event.target));
+    }).on('complete', function() {
+        log('Fastest is ' + this.filter('fastest').map('name'));
+    }).run({
+        'async': true
+    });
+}
